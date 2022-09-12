a0933dd8f9d186edd61dad1ad9fb984dfa073f33
opi <64263323+opibarua1111@users.noreply.github.com>
2022-09-12 21:43:23 +0600

    using some validations

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -50,7 +50,8 @@ let users = [
 ];
 
 module.exports.getAllUsers = (req, res, next) => {
-  res.json(users);
+  const { limit } = req.query;
+  res.json(users.slice(0, limit));
 };
 
 module.exports.getRandomsingleUser = (req, res, next) => {
@@ -60,12 +61,37 @@ module.exports.getRandomsingleUser = (req, res, next) => {
 };
 
 module.exports.saveAUser = (req, res) => {
-  console.log(req.body);
-  users.push(req.body);
-  res.send(users);
+  const user = req.body;
+  if (
+    user.id &&
+    user.gender &&
+    user.name &&
+    user.contact &&
+    user.address &&
+    user.photoUrl
+  ) {
+    users.push(req.body);
+    res.send(users);
+  } else {
+    if (!user.id) {
+      res.send("Id is required");
+    } else if (!user.gender) {
+      res.send("Gender is required");
+    } else if (!user.name) {
+      res.send("Name is required");
+    } else if (!user.contact) {
+      res.send("Contact number is required");
+    } else if (!user.address) {
+      res.send("Address is required");
+    } else if (!user.photoUrl) {
+      res.send("Photo Url is required");
+    }
+  }
 };
 
 function updateUsers(Id, data) {
+  // const Length = data.length ? data.length : 1;
+  // console.log("length :" + Length);
   for (let i = 0; i < data.length; i++) {
     const id = Id ? Id : data[i].id;
     const updateUser = users.find((user) => user.id === Number(id));
@@ -83,41 +109,31 @@ function updateUsers(Id, data) {
 
 module.exports.updateUser = (req, res) => {
   const { id } = req.params;
-  updateUsers(id, req.body);
-  // const updateUser = users.find((user) => user.id === Number(id));
-  // updateUser.id = id;
-  // updateUser.gender = req.body.gender ? req.body.gender : updateUser.gender;
-  // updateUser.name = req.body.name ? req.body.name : updateUser.name;
-  // updateUser.contact = req.body.contact ? req.body.contact : updateUser.contact;
-  // updateUser.address = req.body.address ? req.body.address : updateUser.address;
-  // updateUser.photoUrl = req.body.photoUrl
-  //   ? req.body.photoUrl
-  //   : updateUser.photoUrl;
-  res.send(users);
+  if (!isNaN(id)) {
+    updateUsers(id, [req.body]);
+    res.send(users);
+  } else {
+    res.send(`${id} is not a number`);
+  }
 };
 
 module.exports.updateMultipleUsers = (req, res) => {
   const data = req.body;
+  for (let i = 0; i < data.length; i++) {
+    if (isNaN(data[i].id)) {
+      res.send(`${data[i].id} is not a number`);
+    }
+  }
   updateUsers(null, data);
-
-  // for (let i = 0; i < data.length; i++) {
-  //   const id = data[i].id;
-  //   const updateUser = users.find((user) => user.id === Number(id));
-  //   updateUser.id = id;
-  //   updateUser.gender = data[i].gender ? data[i].gender : updateUser.gender;
-  //   updateUser.name = data[i].name ? data[i].name : updateUser.name;
-  //   updateUser.contact = data[i].contact ? data[i].contact : updateUser.contact;
-  //   updateUser.address = data[i].address ? data[i].address : updateUser.address;
-  //   updateUser.photoUrl = data[i].photoUrl
-  //     ? data[i].photoUrl
-  //     : updateUser.photoUrl;
-  //   console.log(updateUser);
-  // }
   res.send(users);
 };
 
 module.exports.deleteUser = (req, res) => {
   const { id } = req.params;
-  users = users.filter((user) => user.id !== Number(id));
-  res.send(users);
+  if (!isNaN(id)) {
+    users = users.filter((user) => user.id !== Number(id));
+    res.send(users);
+  } else {
+    res.send(`${id} is not a number`);
+  }
 };
